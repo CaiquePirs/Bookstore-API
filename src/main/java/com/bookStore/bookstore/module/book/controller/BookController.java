2bb67323af9a7c4f673f59ddf774c924d5ae988b
Caique Pires <c.pires@ba.estudante.senai.br>
2025-06-07 15:16:48 +0100

    Creating the books API endpoint that creates a book in the database

--- src/main/java/com/bookStore/bookstore/module/book/controller/BookController.java
+++ src/main/java/com/bookStore/bookstore/module/book/controller/BookController.java
@@ -1,8 +1,12 @@
 package com.bookStore.bookstore.module.book.controller;
 
 import com.bookStore.bookstore.module.book.DTO.BookDTO;
+import com.bookStore.bookstore.module.book.mapper.BookMapper;
 import com.bookStore.bookstore.module.book.model.Book;
 import com.bookStore.bookstore.module.book.service.BookService;
+import com.bookStore.bookstore.module.common.error.ErrorResponse;
+import com.bookStore.bookstore.module.common.exception.DuplicateRecordException;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,17 +22,26 @@ import java.net.URI;
 public class BookController {
 
     @Autowired
-    BookService service;
+    private BookService service;
+
+    @Autowired
+    private BookMapper mapper;
 
     @PostMapping
-    public ResponseEntity<Book> create(@RequestBody BookDTO bookDTO){
-       var book = service.create(bookDTO);
+    public ResponseEntity<Object> create(@RequestBody @Valid BookDTO bookDTO){
+       try {
+          Book book = service.create(bookDTO);
 
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(book.getID())
-                .toUri();
+           URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                   .path("/{id}")
+                   .buildAndExpand(book.getId())
+                   .toUri();
 
-        return ResponseEntity.created(uri).body(book);
+           return ResponseEntity.created(uri).body(mapper.toDTO(book));
+
+       } catch (DuplicateRecordException e) {
+        var errorDTO = ErrorResponse.conflict(e.getMessage());
+        return ResponseEntity.status(errorDTO.status()).body(errorDTO);
+    }
     }
 }
