f180121aee5b38e26374e6d238b23c4ed65599b9
Caique Pires <c.pires@ba.estudante.senai.br>
2025-06-08 19:00:34 +0100

    Created the endpoint for the book API to update a book in the database

--- src/main/java/com/bookStore/bookstore/module/book/controller/BookController.java
+++ src/main/java/com/bookStore/bookstore/module/book/controller/BookController.java
@@ -1,5 +1,7 @@
 package com.bookStore.bookstore.module.book.controller;
 
+import com.bookStore.bookstore.module.author.exception.AuthorNotFoundException;
+import com.bookStore.bookstore.module.author.service.AuthorService;
 import com.bookStore.bookstore.module.book.DTO.BookDTO;
 import com.bookStore.bookstore.module.book.DTO.ResponseBookDTO;
 import com.bookStore.bookstore.module.book.exception.BookNotFoundException;
@@ -21,6 +23,7 @@ public class BookController implements GenericController {
 
     private final BookService service;
     private final BookMapper mapper;
+    private final AuthorService authorService;
 
     @PostMapping
     public ResponseEntity<Object> create(@RequestBody @Valid BookDTO bookDTO){
@@ -31,7 +34,7 @@ public class BookController implements GenericController {
 
     @GetMapping("{id}")
     public ResponseEntity<ResponseBookDTO> searchBook(@PathVariable UUID id){
-        return service.searchById(id)
+        return service.getById(id)
                 .map(book -> {
                     var dto = mapper.toDTO(book);
                     return ResponseEntity.ok(dto);
@@ -39,11 +42,33 @@ public class BookController implements GenericController {
     }
     @DeleteMapping("{id}")
     public ResponseEntity<Object> deleteById(@PathVariable UUID id){
-        return service.searchById(id).
+        return service.getById(id).
                 map(book -> {
                     service.deleteById(id);
                     return ResponseEntity.noContent().build();
         }).orElseThrow(() -> new BookNotFoundException(id));
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<ResponseBookDTO> update(@PathVariable UUID id, @RequestBody BookDTO dto){
+        return service.getById(id).map(book -> {
+            service.searchISBN(book);
+
+            var author = authorService.searchById(dto.authorId())
+                    .orElseThrow(() -> new AuthorNotFoundException(dto.authorId()));
+
+            book.setIsbn(dto.isbn());
+            book.setTitle(dto.title());
+            book.setPublisher(dto.publisher());
+            book.setPublicationDate(dto.publicationDate());
+            book.setAuthor(author);
+
+            var updatedBook = service.update(book);
+            return ResponseEntity.ok(mapper.toDTO(updatedBook));
+
+        }).orElseThrow(() -> new BookNotFoundException(id));
+
+
     }
 
 }
