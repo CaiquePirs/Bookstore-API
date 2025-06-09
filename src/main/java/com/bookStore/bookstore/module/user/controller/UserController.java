e1508c35d1b42870f2c3bd1d0aa8d385a92ba313
Caique Pires <c.pires@ba.estudante.senai.br>
2025-06-09 17:46:37 +0100

    created the API Users endpoints to perform CRUD operations on the database, register, search, update and delete. Following good practices and applying validations

---
+++ src/main/java/com/bookStore/bookstore/module/user/controller/UserController.java
@@ -0,0 +1,80 @@
+package com.bookStore.bookstore.module.user.controller;
+
+import com.bookStore.bookstore.module.user.DTO.UserDTO;
+import com.bookStore.bookstore.module.user.DTO.UserResponseDTO;
+import com.bookStore.bookstore.module.user.exception.UserNotFoundException;
+import com.bookStore.bookstore.module.user.mappers.UserMapper;
+import com.bookStore.bookstore.module.user.model.User;
+import com.bookStore.bookstore.module.user.service.UserService;
+import com.bookStore.bookstore.module.user.validator.UserValidator;
+import com.bookStore.bookstore.module.util.GenericController;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController implements GenericController {
+
+    private final UserService service;
+    private final UserMapper mapper;
+    private final UserValidator validator;
+
+    @PostMapping
+    public ResponseEntity<UserResponseDTO> create(@RequestBody @Valid UserDTO dto){
+        var user = service.create(mapper.toEntity(dto));
+        var uri = generateHeaderLocation(user.getId());
+        return ResponseEntity.created(uri).body(mapper.toDTO(user));
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<UserResponseDTO> searchById(@PathVariable UUID id){
+       return service.searchById(id).map(user -> {
+           UserResponseDTO dto = mapper.toDTO(user);
+           return ResponseEntity.ok(dto);
+       }).orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<UserResponseDTO>> listUser(
+            @RequestParam(value = "username", required = false) String username,
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "size-page", defaultValue = "10") Integer sizePage) {
+
+        Page<User> pageResult = service.searchUserByQuery(username, page, sizePage);
+        Page<UserResponseDTO> result = pageResult.map(mapper::toDTO);
+        return ResponseEntity.ok(result);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> delete(@PathVariable UUID id){
+       return service.searchById(id).map(user -> {
+           service.delete(id);
+           return ResponseEntity.ok("User deleted sucefully");
+       }).orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+
+    @PutMapping("{id}")
+    public ResponseEntity<UserResponseDTO> update(@PathVariable UUID id, @RequestBody UserDTO dto){
+        return service.searchById(id).map(user -> {
+            user.setUsername(dto.username());
+            user.setEmail(dto.email());
+            user.setPassword(dto.password());
+
+            validator.validateUser(user);
+            service.update(user);
+
+            var uri = generateHeaderLocation(id);
+            return  ResponseEntity.created(uri).body(mapper.toDTO(user));
+        }).orElseThrow(() -> new UserNotFoundException(id));
+
+
+    }
+
+}
