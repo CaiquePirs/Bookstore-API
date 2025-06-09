6c4f253825335d578bf6add573cde49b6b42d85b
Caique Pires <c.pires@ba.estudante.senai.br>
2025-06-09 17:43:37 +0100

    creating the service class responsible for:
    - creating a user in the database
    
    - searching for a user in the database by ID
    
    - deleting a user in the database by ID
    
    - searching for users in the database by username or all or by the pagination method

---
+++ src/main/java/com/bookStore/bookstore/module/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.bookStore.bookstore.module.user.repository;
+
+import com.bookStore.bookstore.module.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID>, JpaSpecificationExecutor<User> {
+
+    Optional<User> findByEmailOrUsername(String email, String username);
+}
---
+++ src/main/java/com/bookStore/bookstore/module/user/service/UserService.java
@@ -0,0 +1,54 @@
+package com.bookStore.bookstore.module.user.service;
+
+import com.bookStore.bookstore.module.user.mappers.UserMapper;
+import com.bookStore.bookstore.module.user.model.User;
+import com.bookStore.bookstore.module.user.repository.UserRepository;
+import com.bookStore.bookstore.module.user.repository.UserSpecs;
+import com.bookStore.bookstore.module.user.validator.UserValidator;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+
+    private final UserRepository repository;
+    private final UserValidator validator;
+    private final UserMapper mapper;
+
+    public User create(User user){
+        validator.validateUser(user);
+        return repository.save(user);
+    }
+
+    public Optional<User> searchById(UUID id){
+        return repository.findById(id);
+    }
+
+    public void delete(UUID id){
+        repository.deleteById(id);
+    }
+
+    public Page<User> searchUserByQuery(String username, Integer page, Integer sizePage){
+        Specification<User> specs = (root, query, cb) -> cb.conjunction(); // true
+
+        if (username != null && !username.isBlank()) {
+            specs = specs.and(UserSpecs.usernameEqual(username));
+        }
+
+        Pageable pageRequest = PageRequest.of(page, sizePage);
+        return repository.findAll(specs, pageRequest);
+    }
+
+    public User update(User user){
+        return repository.save(user);
+    }
+
+}
