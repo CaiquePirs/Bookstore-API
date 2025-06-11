19538ca397d7cb7f2753c307112353dcb7ae3ef7
Caique Pires <c.pires@ba.estudante.senai.br>
2025-06-11 19:52:18 +0100

    Adding more field in orderDTO to receive more data when updated

--- src/main/java/com/bookStore/bookstore/module/order/DTO/OrderDTO.java
+++ src/main/java/com/bookStore/bookstore/module/order/DTO/OrderDTO.java
@@ -1,11 +1,16 @@
 package com.bookStore.bookstore.module.order.DTO;
 
+import com.bookStore.bookstore.module.book.model.StatusBook;
+import com.bookStore.bookstore.module.order.model.StatusOrder;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.UUID;
 
-public record OrderDTO(@NotNull(message = "Book ID is required")
+public record OrderDTO(UUID id,
+                       @NotNull(message = "Book ID is required")
                        UUID bookId,
                        @NotNull(message = "User ID is required")
-                       UUID userId) {
+                       UUID userId,
+                       StatusOrder statusOrder,
+                       StatusBook statusBook){
 }
