ce64d69ab770edd610f1d019d5fae95ddd9c7c29
Caique Pires <c.pires@ba.estudante.senai.br>
2025-06-16 19:46:12 +0100

    feat(security): allow unauthenticated client registration and enable HTTP Basic Auth
    - Allows public requests for POST /clients (new user registration)
    - Adds support for authentication via HTTP Basic
    - Maintains login with form and protection of other routes via authentication

---
+++ src/main/java/com/bookStore/bookstore/config/SecurityConfiguration.java
@@ -0,0 +1,51 @@
+package com.bookStore.bookstore.config;
+
+import com.bookStore.bookstore.module.client.service.ClientService;
+import com.bookStore.bookstore.security.CustomUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, UserDetailsService userDetailsService) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/clients").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .userDetailsService(userDetailsService)
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(ClientService clientService) {
+        return new CustomUserDetailsService(clientService);
+    }
+}
+
+
+
