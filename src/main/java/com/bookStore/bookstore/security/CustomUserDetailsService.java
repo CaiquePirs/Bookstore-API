bee15e65bdfa182006d6cd91b2ceb05ab046c0c6
Caique Pires <c.pires@ba.estudante.senai.br>
2025-06-16 19:49:37 +0100

    Creates a service that loads the user by username via ClientService for use in Spring Security.

---
+++ src/main/java/com/bookStore/bookstore/security/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.bookStore.bookstore.security;
+
+import com.bookStore.bookstore.module.client.model.Client;
+import com.bookStore.bookstore.module.client.service.ClientService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final ClientService service;
+
+    public CustomUserDetailsService(ClientService service) {
+        this.service = service;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Client client = service.getClientByUsername(username);
+
+        return org.springframework.security.core.userdetails.User
+                .builder()
+                .username(client.getUsername())
+                .password(client.getPassword())
+                .roles(client.getRole().name())
+                .build();
+    }
+}
+
+
