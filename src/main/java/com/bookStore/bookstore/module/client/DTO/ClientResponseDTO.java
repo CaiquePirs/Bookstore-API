073275c19808ad1e776d2d8fd634458092b7511c
Caique Pires <c.pires@ba.estudante.senai.br>
2025-06-21 14:45:25 +0100

    Fix: Removing the encrypted password from the ClientResponseDTO that came when registering a client

--- src/main/java/com/bookStore/bookstore/module/client/DTO/ClientResponseDTO.java
+++ src/main/java/com/bookStore/bookstore/module/client/DTO/ClientResponseDTO.java
@@ -1,12 +1,8 @@
 package com.bookStore.bookstore.module.client.DTO;
 
-
-import java.time.LocalDate;
 import java.util.UUID;
 
 public record ClientResponseDTO(UUID id,
                                 String username,
-                                String email,
-                                String password,
-                                LocalDate dateBirth)  {
+                                String email)  {
 }
